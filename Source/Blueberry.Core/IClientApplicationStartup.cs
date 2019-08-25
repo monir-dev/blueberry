ad5dd7a74691ab409f80321e68f515a0c80cc18e
Md. Monir Hossain <echomonir@gmail.com>
2019-08-25 16:26:21 +0600

    The base starting point for Blueberry Core

---
+++ Source/Blueberry.Core/IClientApplicationStartup.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Blueberry.Core
+{
+    /// <summary>
+    /// The entry point into the Blueberry application used to setup the environment
+    /// All top level applications should create an instance of this
+    /// </summary>
+    public class IClientApplicationStartup
+    {
+    }
+}
